d7d2806ca629efdfe299ce898c5c056b20060834
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-03-29 20:19:55 +0200

    Add HSV image model

---
+++ ImageEditor.Library/Model/HSVImage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Model
+{
+    public class HSVImage
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public float[,] Hue { get; set; }
+        public float[,] Saturation { get; set; }
+        public float[,] Value { get; set; }
+    }
+}
