febad5fe7d95cdea8917c05ed806ce1462d5b8d9
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-03-29 20:15:29 +0200

    Add RGB image model

---
+++ ImageEditor.Library/Model/RGBImage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Model
+{
+    public class RGBImage
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public int[,] R { get; set; }
+        public int[,] G { get; set; }
+        public int[,] B { get; set; }
+
+        public RGBImage(int x, int y)
+        {
+            R = new int[x, y];
+            G = new int[x, y];
+            B = new int[x, y];
+        }
+
+        public RGBImage(int x, int y, int[,] r, int[,] g, int[,] b)
+        {
+            X = x;
+            Y = y;
+            R = r;
+            G = g;
+            B = b;
+        }
+    }
+}
