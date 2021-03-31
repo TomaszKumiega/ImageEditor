10ce75b0d9edc342dde6e100675484ba20e2cd37
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-03-31 12:41:50 +0200

    Add lighting enhancement algorithms class

---
+++ ImageEditor.Library/Algorithms/LightingEnhancementAlgorithms.cs
@@ -0,0 +1,12 @@
+﻿using ImageEditor.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Algorithms
+{
+    public class LightingEnhancementAlgorithms : ILightingEnhancementAlgorithms
+    {
+
+    }
+}
