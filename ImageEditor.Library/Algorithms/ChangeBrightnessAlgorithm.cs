fb7971b1bae132326092b29ea667e6d425ce6714
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-12 20:21:10 +0200

    Move change brightness algorithm to diffrent class

---
+++ ImageEditor.Library/Algorithms/ChangeBrightnessAlgorithm.cs
@@ -0,0 +1,27 @@
+﻿using ImageEditor.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Algorithms
+{
+    public class ChangeBrightnessAlgorithm : IChangeBrightnessAlgorithm
+    {
+        public HSVImage ChangeBrightness(HSVImage image, float brightnessDifference)
+        {
+            var result = new HSVImage(image.X, image.Y);
+
+            for (int x = 0; x < image.X; x++)
+            {
+                for (int y = 0; y < image.Y; y++)
+                {
+                    result.Hue[x, y] = image.Hue[x, y];
+                    result.Saturation[x, y] = image.Saturation[x, y];
+                    result.Value[x,y] = image.Value[x, y] + brightnessDifference;
+                }
+            }
+
+            return result;
+        }
+    }
+}
---
+++ ImageEditor.Library/Algorithms/IChangeBrightnessAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using ImageEditor.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Algorithms
+{
+    public interface IChangeBrightnessAlgorithm
+    {
+        HSVImage ChangeBrightness(HSVImage image, float brightnessDifference);
+    }
+}
