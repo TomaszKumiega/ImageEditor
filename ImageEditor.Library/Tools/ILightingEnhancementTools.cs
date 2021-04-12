6924c315cb0488efc060bc22c195b7bd4f2b4faf
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-12 20:42:03 +0200

    Add LightingEnhancementTools class

---
+++ ImageEditor.Library/Tools/ILightingEnhancementTools.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageEditor.Library.Tools
+{
+    public interface ILightingEnhancementTools
+    {
+        Bitmap ChangeBrightness(Bitmap image, float brightness);
+    }
+}
---
+++ ImageEditor.Library/Tools/LightingEnhancementTools.cs
@@ -0,0 +1,30 @@
+﻿using ImageEditor.Library.Algorithms;
+using ImageEditor.Library.Converter;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageEditor.Library.Tools
+{
+    public class LightingEnhancementTools : ILightingEnhancementTools
+    {
+        private IImageConverter Converter { get; }
+        private IChangeBrightnessAlgorithm ChangeBrightnessAlgorithm { get; }
+
+        public LightingEnhancementTools(IImageConverter converter, IChangeBrightnessAlgorithm changeBrightnessAlgorithm)
+        {
+            Converter = converter;
+            ChangeBrightnessAlgorithm = changeBrightnessAlgorithm;
+        }
+
+        public Bitmap ChangeBrightness(Bitmap image, float brightness)
+        {
+            var hsvImage = Converter.BitmapToHSVImage(image);
+            var hsvResult = ChangeBrightnessAlgorithm.ChangeBrightness(hsvImage, brightness);
+            var result = Converter.HSVImageToBitmap(hsvResult);
+
+            return result;
+        }
+    }
+}
