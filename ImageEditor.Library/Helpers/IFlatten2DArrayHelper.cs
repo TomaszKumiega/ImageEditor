b42f541f5583b942b270207fcea7220f0c04d3d5
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-26 21:16:08 +0200

    Add Flatten2DArrayHelper class

---
+++ ImageEditor.Library/Helpers/IFlatten2DArrayHelper.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Helpers
+{
+    public interface IFlatten2DArrayHelper<T>
+    {
+        T[] Flatten2DArray(T[,] array);
+    }
+}
