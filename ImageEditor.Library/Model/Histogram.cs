b58be76b5a95ba350ede0e37bc18e2152ec7f27a
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-26 20:39:36 +0200

    Add IntensityHistogram class

---
+++ ImageEditor.Library/Model/Histogram.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Model
+{
+    public abstract class Histogram<XType>
+    {
+        public XType[] X { get; set; }
+        public int[] Y { get; set; }
+        public int NumberOfValues { get; set; }
+    }
+}
