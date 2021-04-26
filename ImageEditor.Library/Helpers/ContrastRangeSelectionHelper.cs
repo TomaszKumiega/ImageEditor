489cd7fa1a3867c28a451b62b1e29b40d35a6337
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-26 20:58:30 +0200

    Add ContrastRangeSelectionHelper class

---
+++ ImageEditor.Library/Helpers/ContrastRangeSelectionHelper.cs
@@ -0,0 +1,36 @@
+﻿using ImageEditor.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Helpers
+{
+    public class ContrastRangeSelectionHelper : IContrastRangeSelectionHelper
+    {
+        public void ContrastStretchingRangeSelection(IntensityHistogram histogram, out float minRange, out float maxRange)
+        {
+            float fraction = 0.3f;
+
+            var expectedNumberOfCutValues = histogram.NumberOfValues * fraction;
+            var numberOfCutValues = 0;
+
+            minRange = 0;
+            maxRange = 0;
+
+            for(int i=0;i<histogram.Y.Length/2;i++)
+            {
+                numberOfCutValues += histogram.Y[i];
+
+                if (numberOfCutValues >= expectedNumberOfCutValues) break;
+
+                minRange = histogram.X[i];
+
+                numberOfCutValues += histogram.Y[histogram.Y.Length - i - 1];
+
+                if (numberOfCutValues > expectedNumberOfCutValues) break;
+
+                maxRange = histogram.X[histogram.X.Length - i - 1];
+            }
+        }
+    }
+}
