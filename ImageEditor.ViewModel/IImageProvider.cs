ab9dc44361e7dec0d8a17a21ddfb0e7fd581ccd5
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-28 11:45:53 +0200

    Add ImageProvider class

---
+++ ImageEditor.ViewModel/IImageProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageEditor.ViewModel
+{
+    public interface IImageProvider
+    {
+        Bitmap OriginalImage { get; set; }
+        Bitmap EditedImage { get; set; }
+    }
+}
---
+++ ImageEditor.ViewModel/ImageProvider.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageEditor.ViewModel
+{
+    public class ImageProvider : IImageProvider
+    {
+        public Bitmap OriginalImage { get; set; }
+        public Bitmap EditedImage { get; set; }
+    }
+}
