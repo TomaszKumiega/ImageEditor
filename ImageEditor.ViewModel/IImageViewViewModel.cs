0e698263e1fa5a61ccb88d857b51d81f077a28ca
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-28 12:08:29 +0200

    Add ImageViewViewModel

---
+++ ImageEditor.ViewModel/IImageViewViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.ViewModel
+{
+    public interface IImageViewViewModel
+    {
+        IImageProvider ImageProvider { get; }
+    }
+}
---
+++ ImageEditor.ViewModel/ImageViewViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.ViewModel
+{
+    public class ImageViewViewModel : IImageViewViewModel
+    {
+        public IImageProvider ImageProvider { get; }
+
+        public ImageViewViewModel(IImageProvider imageProvider)
+        {
+            ImageProvider = imageProvider;
+        }
+    }
+}
