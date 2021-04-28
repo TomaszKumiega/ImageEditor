0e251c3e2302917bb8797cca3944d6288e6ee2fc
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-28 12:21:56 +0200

    Add LightingEnhancementToolsViewModel class

---
+++ ImageEditor.ViewModel/LigthingEnhancementToolsViewModel.cs
@@ -0,0 +1,54 @@
+﻿using ImageEditor.Library.Tools;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.ViewModel
+{
+    public class LigthingEnhancementToolsViewModel : ILightingEnhancementToolsViewModel
+    {
+        private IImageProvider ImageProvider { get; }
+        private ILightingEnhancementTools LightingEnhancementTools { get; }
+        private float _contrast;
+        private float _brightness;
+
+        public float Contrast 
+        { 
+            get => _contrast; 
+            set
+            {
+                if (value != 0) ChangeContrast(value);
+                _contrast = value;
+            }
+        }
+
+        public float Brightness 
+        
+        {
+            get => _brightness; 
+            set
+            {
+                if (value != 0) ChangeBrightness(value);
+                _brightness = value;
+            }
+        }
+
+        public LigthingEnhancementToolsViewModel(IImageProvider imageProvider, ILightingEnhancementTools lightingEnhancementTools)
+        {
+            LightingEnhancementTools = lightingEnhancementTools;
+            ImageProvider = imageProvider;
+            _contrast = 0;
+            _brightness = 0;
+        }
+
+        private void ChangeContrast(float contrast)
+        {
+            ImageProvider.EditedImage = LightingEnhancementTools.ChangeContrast(ImageProvider.EditedImage, contrast);
+        }
+
+        private void ChangeBrightness(float brightness)
+        {
+            ImageProvider.EditedImage = LightingEnhancementTools.ChangeBrightness(ImageProvider.EditedImage, brightness);
+        }
+    }
+}
