e7c670144a9592ac81f405f47144b9f4cbd98626
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-04-28 11:55:59 +0200

    Add ImageIO class

---
+++ ImageEditor.Library/Helpers/IImageIO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageEditor.Library.Helpers
+{
+    public interface IImageIO
+    {
+        void SaveImage(Bitmap bitmap, string path);
+        Bitmap LoadImage(string path);
+    }
+}
---
+++ ImageEditor.Library/Helpers/ImageIO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Text;
+
+namespace ImageEditor.Library.Helpers
+{
+    public class ImageIO : IImageIO
+    {
+        public Bitmap LoadImage(string path)
+        {
+            return new Bitmap(path);
+        }
+
+        public void SaveImage(Bitmap bitmap, string path)
+        {
+            var stream = File.Open(path, FileMode.OpenOrCreate, FileAccess.Write);
+            bitmap.Save(stream, bitmap.RawFormat);
+            stream.Close();
+        }
+    }
+}
