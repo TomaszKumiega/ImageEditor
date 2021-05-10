81ac6038a7b8517a50592888a3e494b5cc1b1543
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-10 20:53:00 +0200

    Add ImageConverter

---
+++ ImageEditor.WPF.UserControls/Helpers/ImageConverter.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Globalization;
+using System.IO;
+using System.Text;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace ImageEditor.WPF.UserControls.Helpers
+{
+    public class ImageConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var bitmap = (Bitmap) value;
+            var result = new BitmapImage();
+
+            if (bitmap == null) return null;
+
+            using(MemoryStream ms = new MemoryStream())
+            {
+                bitmap.Save(ms, bitmap.RawFormat);
+                ms.Seek(0, SeekOrigin.Begin);
+                result.BeginInit();
+                result.CacheOption = BitmapCacheOption.OnLoad;
+                result.StreamSource = ms;
+                result.EndInit();
+                return result;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+    }
+}
