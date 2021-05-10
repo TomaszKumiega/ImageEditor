33d153687f15e1d1a1418c741f66f82a3b34e545
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-10 19:56:37 +0200

    Add ImageViewUserControl

---
+++ ImageEditor.WPF.UserControls/ImageViewUserControl.xaml.cs
@@ -0,0 +1,28 @@
+﻿using ImageEditor.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ImageEditor.WPF.UserControls
+{
+    /// <summary>
+    /// Interaction logic for ImageViewUserControl.xaml
+    /// </summary>
+    public partial class ImageViewUserControl : UserControl
+    {
+        public ImageViewUserControl(IImageViewViewModel viewModel)
+        {
+            DataContext = viewModel;
+            InitializeComponent();
+        }
+    }
+}
