888886299f087f360e883d036bf7452ee0492f70
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-10 22:18:51 +0200

    Implement INotifyPropertyChanged interface

--- ImageEditor.ViewModel/ImageProvider.cs
+++ ImageEditor.ViewModel/ImageProvider.cs
@@ -1,14 +1,50 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Drawing;
 using System.Text;
 
 namespace ImageEditor.ViewModel
 {
-    public class ImageProvider : IImageProvider
+    public class ImageProvider : IImageProvider, INotifyPropertyChanged
     {
-        public Bitmap OriginalImage { get; set; }
-        public Bitmap EditedImage { get; set; }
-        public string ImagePath { get; set; }
+        private Bitmap _originalImage;
+        private Bitmap _editedImage;
+        private string _imagePath;
+
+        public Bitmap OriginalImage 
+        {
+            get => _originalImage;
+            set
+            {
+                _originalImage = value;
+                OnPropertyChanged("OriginalImage");
+            }
+        }
+        public Bitmap EditedImage 
+        { 
+            get => _editedImage; 
+            set
+            {
+                _editedImage = value;
+                OnPropertyChanged("EditedImage");
+            }
+        }
+        public string ImagePath 
+        {
+            get => _imagePath; 
+            set
+            {
+                _imagePath = value;
+                OnPropertyChanged("ImagePath");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
     }
 }
