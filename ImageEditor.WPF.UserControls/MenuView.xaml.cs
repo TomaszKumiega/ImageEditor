6098d84128d2d020f9744f433cd72df84ba29147
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-10 21:43:01 +0200

    Add SaveImageAs method

--- ImageEditor.WPF.UserControls/MenuView.xaml.cs
+++ ImageEditor.WPF.UserControls/MenuView.xaml.cs
@@ -1,15 +1,19 @@
-﻿using System;
+﻿using ImageEditor.ViewModel;
+using ImageEditor.WPF.UserControls.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using UserControl = System.Windows.Controls.UserControl;
 
 namespace ImageEditor.WPF.UserControls
 {
@@ -18,9 +22,22 @@ namespace ImageEditor.WPF.UserControls
     /// </summary>
     public partial class MenuView : UserControl
     {
-        public MenuView()
+        public MenuView(IMenuViewModel viewModel)
         {
+            DataContext = viewModel;
             InitializeComponent();
+        }
+
+        private void SaveAsImageButton_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new SaveFileDialog();
+            DialogResult result = dialog.ShowDialog(this.GetIWin32Window());
+            var viewModel = DataContext as IMenuViewModel; 
+
+            if(result == System.Windows.Forms.DialogResult.OK)
+            {
+                viewModel.SaveImageAs(dialog.FileName);
+            }
         }
     }
 }
