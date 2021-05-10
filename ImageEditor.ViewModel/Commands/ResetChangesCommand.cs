13d51eae2911df37ccf3d9cf5f56bd08d2a02202
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-10 21:59:50 +0200

    Add ResetChangesCommand

---
+++ ImageEditor.ViewModel/Commands/ResetChangesCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ImageEditor.ViewModel.Commands
+{
+    public class ResetChangesCommand : ICommand
+    {
+        private IMenuViewModel ViewModel { get; }
+        public event EventHandler CanExecuteChanged;
+
+        public ResetChangesCommand(IMenuViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModel.Reset();
+        }
+    }
+}
