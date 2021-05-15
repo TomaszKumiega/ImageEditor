fbcc8dc5ed1bd35b409dbf332ab36f7e421bd8a7
Tomasz Kumiega <tomekkumiega@interia.pl>
2021-05-15 20:48:01 +0200

    Change HSVPixelToColor implementation

--- ImageEditor.Library/Converter/ImageConverter.cs
+++ ImageEditor.Library/Converter/ImageConverter.cs
@@ -86,42 +86,29 @@ namespace ImageEditor.Library.Converter
             return rgbImage;
         }
 
-        public Color HSVPixelToColor(float h, float s, float v)
+        public Color HSVPixelToColor(float hue, float saturation, float value)
         {
-            int hi = (int)Math.Floor(h / 60.0) % 6;
-            double f = (h / 60.0) - Math.Floor(h / 60.0);
+            int hi = Convert.ToInt32(Math.Floor(hue / 60)) % 6;
+            double f = hue / 60 - Math.Floor(hue / 60);
 
-            double p = v * (1.0 - s);
-            double q = v * (1.0 - (f * s));
-            double t = v * (1.0 - ((1.0 - f) * s));
+            value = value * 255;
+            int v = Convert.ToInt32(value);
+            int p = Convert.ToInt32(value * (1 - saturation));
+            int q = Convert.ToInt32(value * (1 - f * saturation));
+            int t = Convert.ToInt32(value * (1 - (1 - f) * saturation));
 
-            Color ret;
-
-            switch (hi)
-            {
-                case 0:
-                    ret = GetRgb(v, t, p);
-                    break;
-                case 1:
-                    ret = GetRgb(q, v, p);
-                    break;
-                case 2:
-                    ret = GetRgb(p, v, t);
-                    break;
-                case 3:
-                    ret = GetRgb(p, q, v);
-                    break;
-                case 4:
-                    ret = GetRgb(t, p, v);
-                    break;
-                case 5:
-                    ret = GetRgb(v, p, q);
-                    break;
-                default:
-                    ret = Color.FromArgb(0xFF, 0x00, 0x00, 0x00);
-                    break;
-            }
-            return ret;
+            if (hi == 0)
+                return Color.FromArgb(255, v, t, p);
+            else if (hi == 1)
+                return Color.FromArgb(255, q, v, p);
+            else if (hi == 2)
+                return Color.FromArgb(255, p, v, t);
+            else if (hi == 3)
+                return Color.FromArgb(255, p, q, v);
+            else if (hi == 4)
+                return Color.FromArgb(255, t, p, v);
+            else
+                return Color.FromArgb(255, v, p, q);
         }
 
         public Bitmap RGBImageToBitmap(RGBImage image)
