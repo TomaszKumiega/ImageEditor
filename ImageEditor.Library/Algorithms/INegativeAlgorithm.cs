8f60b7f9af6b73d6b92399af5e45cb9fd70e5868
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-23 21:18:44 +0200

    Add NegativeAlgorithm

---
+++ ImageEditor.Library/Algorithms/INegativeAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageEditor.Library.Algorithms
+{
+    public interface INegativeAlgorithm
+    {
+        Bitmap ChangeToNegative(Bitmap image);
+    }
+}
---
+++ ImageEditor.Library/Algorithms/NegativeAlgorithm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageEditor.Library.Algorithms
+{
+    public class NegativeAlgorithm : INegativeAlgorithm
+    {
+        public Bitmap ChangeToNegative(Bitmap image)
+        {
+            var clonedImage = (Bitmap)image.Clone();
+
+            Rectangle rect = new Rectangle(0, 0, image.Width, image.Height);
+            System.Drawing.Imaging.BitmapData bitmapData =
+                clonedImage.LockBits(rect, System.Drawing.Imaging.ImageLockMode.ReadWrite, clonedImage.PixelFormat);
+
+            IntPtr ptr = bitmapData.Scan0;
+            int bytes = Math.Abs(bitmapData.Stride) * clonedImage.Height;
+            byte[] rgbValues = new byte[bytes];
+
+            System.Runtime.InteropServices.Marshal.Copy(ptr, rgbValues, 0, bytes);
+
+            for (int i = 0; i < rgbValues.Length; i++)
+            {
+                rgbValues[i] = (byte)(255 - rgbValues[i]);
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(rgbValues, 0, ptr, bytes);
+
+            clonedImage.UnlockBits(bitmapData);
+
+            return clonedImage;
+        }
+    }
+}
