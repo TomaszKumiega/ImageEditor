b9ba01ec99aad07c8529ea3aba52a8bea00ad07b
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-23 19:10:54 +0200

    Add OperationFinishedEventHandler

---
+++ ImageEditor.Library/Events/OperationFinishedEventHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Events
+{
+    public class OperationFinishedEventArgs : EventArgs
+    {
+        public string OperationName { get; }
+
+        public OperationFinishedEventArgs(string operationName)
+        {
+            OperationName = operationName;
+        }
+    }
+
+    public delegate void OperationFinishedEventHandler(object sender, OperationFinishedEventArgs args);
+}
