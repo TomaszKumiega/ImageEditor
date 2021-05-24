e4d67d4532b8c54a07558751dd124077900fbc58
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-24 13:27:36 +0200

    Add HSVColor model

---
+++ ImageEditor.Library/Model/HSVColor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Model
+{
+    public class HSVColor
+    {
+        public float Hue { get; set; }
+        public float Saturation { get; set; }
+        public float Value { get; set; }
+
+        public HSVColor()
+        {
+
+        }
+
+        public HSVColor(float h, float s, float v)
+        {
+            Hue = h;
+            Saturation = s;
+            Value = v;
+        }
+    }
+}
