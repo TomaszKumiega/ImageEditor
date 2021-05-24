7c2171e039fad411d9b6d38bec85159952b2d151
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-24 13:29:40 +0200

    Add RGBColor model

---
+++ ImageEditor.Library/Model/RGBColor.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Model
+{
+    public class RGBColor
+    {
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+
+        public RGBColor()
+        {
+
+        }
+
+        public RGBColor(byte red, byte green, byte blue)
+        {
+            Red = red;
+            Green = green;
+            Blue = blue;
+        }
+
+        public RGBColor(int red, int green, int blue)
+        {
+            Red = (byte)red;
+            Green = (byte)green;
+            Blue = (byte)blue;
+        }
+    }
+}
