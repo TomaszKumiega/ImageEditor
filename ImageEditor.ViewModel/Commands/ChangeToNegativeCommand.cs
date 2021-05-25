de77a333072ac7cf16890e434e8b6812acd213bd
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-25 17:28:22 +0200

    Add ChangeToNegativeCommand

---
+++ ImageEditor.ViewModel/Commands/ChangeToNegativeCommand.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ImageEditor.ViewModel.Commands
+{
+    public class ChangeToNegativeCommand : ICommand
+    {
+        private IEffectsToolsViewModel ViewModel { get; }
+        public event EventHandler CanExecuteChanged;
+
+        public ChangeToNegativeCommand(IEffectsToolsViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModel.ChangeToNegative();
+        }
+    }
+}
