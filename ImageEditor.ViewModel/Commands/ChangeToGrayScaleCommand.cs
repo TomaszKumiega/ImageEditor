bfb5e69c7a04f1c041ad3039f2b40c47d3e6791e
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-25 17:26:30 +0200

    Add ChangeToGrayScaleCommand

---
+++ ImageEditor.ViewModel/Commands/ChangeToGrayScaleCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace ImageEditor.ViewModel.Commands
+{
+    public class ChangeToGrayScaleCommand : ICommand
+    {
+        private IEffectsToolsViewModel ViewModel { get; }
+
+        public event EventHandler CanExecuteChanged;
+
+        public ChangeToGrayScaleCommand(IEffectsToolsViewModel viewModel)
+        {
+            ViewModel = viewModel;
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            ViewModel.ChangeToGrayScale();
+        }
+    }
+}
