f4f3a405eb08838db0cc3362e28706867d205ef0
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-25 20:38:22 +0200

    Add HistogramStatisticsHelper

---
+++ ImageEditor.Library/Helpers/HistogramStatisticsHelper.cs
@@ -0,0 +1,37 @@
+﻿using ImageEditor.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ImageEditor.Library.Helpers
+{
+    public class HistogramStatisticsHelper : IHistogramStatisticsHelper
+    {
+        public (T min, T max) FindBoundariesOfXPercentage<T>(Histogram<T> histogram, float percentage)
+        {
+            var allValues = histogram.CountAll();
+            var numberOfValuesToCut = (int)(allValues - (allValues * percentage));
+            int cutValues = 0;
+            T min = histogram.Keys.First();
+            T max = histogram.Keys.Last();
+
+            for(int i=0; i<histogram.Count; ++i)
+            {
+                var item = histogram.ElementAt(i);
+                cutValues += item.Value;
+
+                if (cutValues >= numberOfValuesToCut) break;
+                min = item.Key;
+
+                item = histogram.ElementAt(histogram.Count - 1 - i);
+                cutValues += item.Value;
+
+                if (cutValues >= numberOfValuesToCut) break;
+                max = item.Key;
+            }
+
+            return (min, max);
+        }
+    }
+}
---
+++ ImageEditor.Library/Helpers/IHistogramStatisticsHelper.cs
@@ -0,0 +1,12 @@
+﻿using ImageEditor.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Helpers
+{
+    public interface IHistogramStatisticsHelper
+    {
+        (T min, T max) FindBoundariesOfXPercentage<T>(Histogram<T> histogram, float percentage);
+    }
+}
