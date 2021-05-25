e6d57e8a75588a7b964f451684d0e782f387697f
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-25 17:00:25 +0200

    Add ChangeSaturationAlgorithm

---
+++ ImageEditor.Library/Algorithms/ChangeSaturationAlgorithm.cs
@@ -0,0 +1,29 @@
+﻿using ImageEditor.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Algorithms
+{
+    public class ChangeSaturationAlgorithm : IChangeSaturationAlgorithm
+    {
+        public HSVImage ChangeSaturation(HSVImage image, float saturationGain)
+        {
+            var hsvImage = new HSVImage(image.X, image.Y);
+
+            for (int x = 0; x < image.X; ++x)
+            {
+                for (int y = 0; y < image.Y; ++y)
+                {
+                    var pixel = image.GetPixel(x, y);
+                    var sat = pixel.Saturation + saturationGain;
+                    sat = sat > 1 ? 1 : sat < 0 ? 0 : sat;
+                    var hsvColor = new HSVColor(pixel.Hue, sat, pixel.Value);
+                    hsvImage.SetPixel(x, y, hsvColor);
+                }
+            }
+
+            return hsvImage;
+        }
+    }
+}
