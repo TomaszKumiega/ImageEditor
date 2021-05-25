b5074a89a763b5d6264f8f9cb5eeb7845996a6b5
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-25 16:53:53 +0200

    Add ChangeTint method

--- ImageEditor.Library/Tools/ColorEnhancementTools.cs
+++ ImageEditor.Library/Tools/ColorEnhancementTools.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using ImageEditor.Library.Algorithms;
+using ImageEditor.Library.Converter;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ImageEditor.Library.Tools
 {
     public class ColorEnhancementTools : IColorEnhancementTools
     {
+        private IImageConverter Converter { get; }
+        private IChangeTintAlgorithm ChangeTintAlgorithm { get; }
+
+        public ColorEnhancementTools(IImageConverter converter, IChangeTintAlgorithm changeTintAlgorithm)
+        {
+            Converter = converter;
+            ChangeTintAlgorithm = changeTintAlgorithm;
+        }
+
+        public Bitmap ChangeTint(Bitmap bitmap, float tint)
+        {
+            var actualTint = (float)(tint * 3.6);
+            var hsvImage = Converter.BitmapToHSVImage(bitmap);
+            var resultHSV = ChangeTintAlgorithm.ChangeTint(hsvImage, actualTint);
+            var result = Converter.HSVImageToBitmap(resultHSV);
+            return result;
+        }
     }
 }
--- ImageEditor.Library/Tools/IColorEnhancementTools.cs
+++ ImageEditor.Library/Tools/IColorEnhancementTools.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ImageEditor.Library.Tools
 {
     public interface IColorEnhancementTools
     {
+        Bitmap ChangeTint(Bitmap bitmap, float tint);
     }
 }
