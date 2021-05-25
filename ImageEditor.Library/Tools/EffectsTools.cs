09818cac70eb0548f750bc9143f4c32c96144f11
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-25 17:35:53 +0200

    Add algorithms to effects tools

--- ImageEditor.Library/Tools/EffectsTools.cs
+++ ImageEditor.Library/Tools/EffectsTools.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using ImageEditor.Library.Algorithms;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ImageEditor.Library.Tools
 {
     public class EffectsTools : IEffectsTools
     {
+        private INegativeAlgorithm NegativeAlgorithm { get; }
+        private IGrayscaleAlgorithm GrayscaleAlgorithm { get; }
+
+        public EffectsTools(INegativeAlgorithm negativeAlgorithm, IGrayscaleAlgorithm grayscaleAlgorithm)
+        {
+            NegativeAlgorithm = negativeAlgorithm;
+            GrayscaleAlgorithm = grayscaleAlgorithm;
+        }
+
+        public Bitmap ChangeToGrayScale(Bitmap bitmap)
+        {
+            return GrayscaleAlgorithm.ChangeToGrayscale(bitmap);
+        }
+
+        public Bitmap ChangeToNegative(Bitmap bitmap)
+        {
+            return NegativeAlgorithm.ChangeToNegative(bitmap);
+        }
     }
 }
--- ImageEditor.Library/Tools/IEffectsTools.cs
+++ ImageEditor.Library/Tools/IEffectsTools.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ImageEditor.Library.Tools
 {
     public interface IEffectsTools
     {
+        Bitmap ChangeToGrayScale(Bitmap bitmap);
+        Bitmap ChangeToNegative(Bitmap bitmap);
     }
 }
