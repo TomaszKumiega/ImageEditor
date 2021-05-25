2a37591d9cf362d2d5afa733637eed0f6d456bab
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-25 16:50:15 +0200

    Add ChangeTint algorithm

---
+++ ImageEditor.Library/Algorithms/ChangeTintAlgorithm.cs
@@ -0,0 +1,28 @@
+﻿using ImageEditor.Library.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.Library.Algorithms
+{
+    public class ChangeTintAlgorithm : IChangeTintAlgorithm
+    {
+        public HSVImage ChangeTint(HSVImage image, float tint)
+        {
+            var hsvImage = new HSVImage(image.X, image.Y);
+
+            for (int x=0; x < image.X; ++x)
+            {
+                for(int y=0; y<image.Y; ++y)
+                {
+                    var pixel = image.GetPixel(x, y);
+                    var h = pixel.Hue + tint;
+                    var hsvColor = new HSVColor(h, pixel.Saturation, pixel.Value);
+                    hsvImage.SetPixel(x, y, hsvColor);
+                }
+            }
+
+            return hsvImage;
+        }
+    }
+}
