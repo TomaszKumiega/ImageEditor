7c3d41728d3358ae74b9dbff8bf84f1f6c80bc80
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-28 15:28:47 +0200

    Add ApplyEvent

---
+++ ImageEditor.ViewModel/Events/ApplyEvent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.ViewModel.Events
+{
+    public class ApplyEventArgs
+    {
+        public Operation Operation { get; }
+
+        public ApplyEventArgs(Operation op)
+        {
+            Operation = op;
+        }
+    }
+        
+
+    public delegate void ApplyEventHandler(object sender, ApplyEventArgs args);
+}
---
+++ ImageEditor.ViewModel/Operation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImageEditor.ViewModel
+{
+    public enum Operation
+    {
+        WhiteBalance,
+        Contrast,
+        Negative,
+        Grayscale,
+        Tint,
+        Saturation,
+        Brightness
+    }
+}
