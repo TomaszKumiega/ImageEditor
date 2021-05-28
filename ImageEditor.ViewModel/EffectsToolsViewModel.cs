13a57ff9d90e91b33472ae9edbfeb302ecc36e06
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-05-28 15:38:32 +0200

    Implement apply event

--- ImageEditor.ViewModel/EffectsToolsViewModel.cs
+++ ImageEditor.ViewModel/EffectsToolsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using ImageEditor.Library.Tools;
 using ImageEditor.ViewModel.Commands;
+using ImageEditor.ViewModel.Events;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -21,14 +22,26 @@ namespace ImageEditor.ViewModel
             EffectsTools = effectsTools;
             ChangeToGrayScaleCommand = commandFactory.GetChangeToGrayScaleCommand(this);
             ChangeToNegativeCommand = commandFactory.GetChangeToNegativeCommand(this);
+            ImageProvider.ApplyEvent += OnApply;
         }
 
-        public void ChangeToGrayScale()
+        private void OnApply(object sender, ApplyEventArgs args)
+        {
+            switch(args.Operation)
+            {
+                case Operation.Grayscale: ChangeToGrayscale();
+                    break;
+                case Operation.Negative: ChangeToNegative();
+                    break;
+            }
+        }
+
+        private void ChangeToGrayscale()
         {
             ImageProvider.EditedImage = EffectsTools.ChangeToGrayScale(ImageProvider.EditedImage);
         }
 
-        public void ChangeToNegative()
+        private void ChangeToNegative()
         {
             ImageProvider.EditedImage = EffectsTools.ChangeToNegative(ImageProvider.EditedImage);
         }
