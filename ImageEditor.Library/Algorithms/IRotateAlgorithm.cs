43fac71335bf08e1617285460a7159662e35d27b
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-05 12:19:35 +0200

    Add RotateAlgorithm

---
+++ ImageEditor.Library/Algorithms/IRotateAlgorithm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageEditor.Library.Algorithms
+{
+    public interface IRotateAlgorithm
+    {
+        Bitmap Rotate(Bitmap bitmap, float angle);
+    }
+}
---
+++ ImageEditor.Library/Algorithms/RotateAlgorithm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace ImageEditor.Library.Algorithms
+{
+    public class RotateAlgorithm : IRotateAlgorithm
+    {
+        public Bitmap Rotate(Bitmap bitmap, float angle)
+        {
+            var resultBitmap = bitmap.Clone(new Rectangle(0, 0, bitmap.Width, bitmap.Height), System.Drawing.Imaging.PixelFormat.Format24bppRgb);
+
+            using(Graphics gdi = Graphics.FromImage(resultBitmap))
+            {
+                gdi.TranslateTransform((float)bitmap.Width / 2, (float)bitmap.Height / 2);
+                gdi.RotateTransform(angle);
+                gdi.TranslateTransform((float)-bitmap.Width / 2, (float)-bitmap.Height / 2);
+            }
+
+            return resultBitmap;
+        }
+    }
+}
