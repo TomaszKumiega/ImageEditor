ed376ce37480dab3d6562a7ae1e54064b8dc4e8f
Tomasz Kumiega <tomasz.mateusz.kumiega@gmail.com>
2021-06-05 12:21:59 +0200

    Add Rotate method to shaping tools

--- ImageEditor.Library/Tools/IShapingTools.cs
+++ ImageEditor.Library/Tools/IShapingTools.cs
@@ -1,10 +1,13 @@
-﻿using System;
+﻿using ImageEditor.Library.Algorithms;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ImageEditor.Library.Tools
 {
     public interface IShapingTools
     {
+        Bitmap Rotate(Bitmap bitmap, float angle);
     }
 }
--- ImageEditor.Library/Tools/ShapingTools.cs
+++ ImageEditor.Library/Tools/ShapingTools.cs
@@ -1,10 +1,23 @@
-﻿using System;
+﻿using ImageEditor.Library.Algorithms;
+using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace ImageEditor.Library.Tools
 {
     public class ShapingTools : IShapingTools
     {
+        private IRotateAlgorithm RotateAlgorithm { get; }
+
+        public ShapingTools(IRotateAlgorithm rotateAlgorithm)
+        {
+            RotateAlgorithm = rotateAlgorithm;
+        }
+
+        public Bitmap Rotate(Bitmap bitmap, float angle)
+        {
+            return RotateAlgorithm.Rotate(bitmap, angle);
+        }
     }
 }
